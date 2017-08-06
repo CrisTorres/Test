01b025c672ae7551e080377dffe33b74dc1d5765
CrisTores <torres_alonso@msn.com>
2017-08-06 19:39:31 +0200

    third commit

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -7,6 +7,10 @@ public class TestClass {
 	public static void main(String[] args){
 		
 		System.out.println("Working");
+		System.out.println("HEY");
+		System.out.println("HEY");
+		System.out.println("HEY");
+
 		return;
 		
 		/*
